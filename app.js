44d1bb1d4afc1c17bedd2af6375371c498e7d9ef
Rajan Kumar <rajanku1920@gmail.com>
2021-10-08 19:02:33 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,77 +1,74 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ =require("lodash"); 
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
-let posts=[];
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://rajan:rajan123@clusterrk-shard-00-00.stdec.mongodb.net:27017,clusterrk-shard-00-01.stdec.mongodb.net:27017,clusterrk-shard-00-02.stdec.mongodb.net:27017/secretsDB?ssl=true&replicaSet=atlas-lw7hmd-shard-0&authSource=admin&retryWrites=true&w=majority",{useNewUrlParser:true, useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+  });
+
+
+  userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
+  const User = mongoose.model("User", userSchema);
+
+
 
 app.get("/",function(req,res){
-res.render("home",{
-  startingContent: homeStartingContent,
-  posts: posts
- 
+    res.render("home");
+});
+app.get("/login",function(req,res){
+    res.render("login");
+});
+app.get("/register",function(req,res){
+    res.render("register");
 });
 
+app.post("/register",function(req,res){
+const newUser = new User ({
+email:req.body.username,
+password:req.body.password
 });
 
-app.get("/about", function(req, res){
-  res.render("about",{aboutContent:aboutContent});
-});
-
-app.get("/contact", function(req, res){
-  res.render("contact",{contactContent:contactContent});
-});
-
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
-
-app.post("/compose", function(req, res){
-  const post={
-    title:req.body.postTitle,
-    content:req.body.postBody
-  }
- posts.push(post);
- 
- res.redirect("/");
-});
-
-app.get("/posts/:postName", function (req, res) {
- const requestedTitle= _.lowerCase(req.params.postName);
-
-posts.forEach(function(post)
-{
-  const storedTitle= _.lowerCase(post.title);
-
-  if(storedTitle===requestedTitle)
-  res.render("post",{
-   
-    title: post.title,
-    content:post.content
-   
-  });
+newUser.save(function(err){
+    if(!err){
+        res.render("secrets");
+    }
+    else{
+        console.log(err);
+    }
 });
 });
+app.post("/login",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
 
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+    User.findOne({email:username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                if(foundUser.password===password){
+                    res.render("secrets");
+                }
+            }
+        }
+    })
 });
+
+app.listen(3000,function(){
+    console.log("Server started on port 3000");
+})
